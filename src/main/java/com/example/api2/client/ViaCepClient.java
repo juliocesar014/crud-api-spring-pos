f6b3ac2aec55f3da37e46193badce32b9b4b13f2
“Júlio <juliocesar014@live.com>
2024-02-23 21:37:23 -0300

    feat: Add exceptions

---
+++ src/main/java/com/example/api2/client/ViaCepClient.java
@@ -0,0 +1,4 @@
+package com.example.api2.client;
+
+public class ViaCepClient {
+}
---
+++ src/main/java/com/example/api2/model/dto/ErrorDTO.java
@@ -0,0 +1,15 @@
+package com.example.api2.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ErrorDTO {
+    private String error;
+    private String details;
+}
